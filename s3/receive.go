2b42631985b642797ef1cbeb9501f3a91f52c84f
Simon Zimmermann <simon@insmo.com>
2014-03-09 09:30:36 +0100

    Correct copyright information for each file.

--- s3/receive.go
+++ s3/receive.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2011 Google Inc.
+Modifications Copyright (c) 2014 Simon Zimmermann
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
