49309c502e4249beb253b3e05297a3756d29695a
Simon Zimmermann <simon@insmo.com>
2014-03-09 11:23:58 +0100

    fix LICENSE in a few files

--- constants.go
+++ constants.go
@@ -1,3 +1,7 @@
+// Copyright 2014 Simon Zimmermann. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package blobserver
 
 // MaxBlobSize is the max size of a single blob. 16MB
